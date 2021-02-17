28f8f90ff26330cdad790abb96cc66c1d6187353
Wojciech Pokrzepa <wojciech.pokrzepa@netent.com>
2021-02-17 23:03:53 +0100

    Add blackjack-service module

---
+++ blackjack-service/src/main/java/io/sgnsajgon/reactspringbootblackjack/blackjackservice/BlackjackServiceApplication.java
@@ -0,0 +1,13 @@
+package io.sgnsajgon.reactspringbootblackjack.blackjackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackServiceApplication.class, args);
+	}
+
+}
---
+++ blackjack-service/src/test/java/io/sgnsajgon/reactspringbootblackjack/blackjackservice/BlackjackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sgnsajgon.reactspringbootblackjack.blackjackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
